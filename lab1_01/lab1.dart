2d13bc1cbbf43fd6bb4f80fe1e557ddab7ac9fd5
Chang <xiangxiang3451@gmail.com>
2024-11-20 19:58:57 +0700

    lab1

--- lab1_01/lab1.dart
+++ lab1_01/lab1.dart
@@ -3,57 +3,48 @@ import 'dart:async';
 import 'dart:io';
 
 Future<void> downloadFile(String url) async {
-  // Разбор URL
+
   final uri = Uri.parse(url);
 
-  // Получение имени файла, по умолчанию "downloaded_file"
   final fileName = uri.pathSegments.isNotEmpty ? uri.pathSegments.last : 'downloaded_file';
   final file = File(fileName);
 
-  // Открытие HTTP-клиента
   final client = HttpClient();
 
-  // Отслеживание количества загруженных байт
   int downloadedBytes = 0;
 
-  // Таймер для вывода прогресса загрузки каждую секунду
   Timer? progressTimer;
   progressTimer = Timer.periodic(Duration(seconds: 1), (timer) {
     print('Загружено: $downloadedBytes байт');
   });
 
   try {
-    // Отправка HTTP GET-запроса
     final request = await client.getUrl(uri);
     final response = await request.close();
 
-    // Проверка статуса ответа
     if (response.statusCode != HttpStatus.ok) {
       throw HttpException('HTTP-запрос не удался, код состояния: ${response.statusCode}');
     }
 
-    // Создание потока для записи в файл
     final sink = file.openWrite();
 
-    // Асинхронное чтение данных из ответа и запись в файл
     await response.listen((chunk) {
       sink.add(chunk);
       downloadedBytes += chunk.length;
-    }).asFuture(); // Ожидание завершения потока
+    }).asFuture(); 
 
-    await sink.close(); // Закрытие потока файла
+    await sink.close(); 
     print('Загрузка завершена, файл сохранён как: $fileName');
   } catch (e) {
     print('Ошибка загрузки: $e');
   } finally {
-    // Остановка таймера прогресса
-    progressTimer?.cancel();
+    progressTimer.cancel();
     client.close();
   }
 }
 
 void main(List<String> arguments) async {
-  // Проверка аргументов командной строки
+
   if (arguments.isEmpty) {
     print('Использование: dart filename.dart <URL>');
     exit(1);
